b46b76ee511e168f65d70fa513fce26f5670fb9b
akhilpkb <79098690+akhilpkb@users.noreply.github.com>
2022-12-26 17:16:00 +0530

    first commit

---
+++ todoapp/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('',views.add,name='add'),
+    path('delete/<int:taskid>/',views.delete,name='delete'),
+    path('update/<int:id>/', views.update, name='update'),
+
+]
